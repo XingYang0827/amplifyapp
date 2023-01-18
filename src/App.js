395fcfaddea2912e499df7aceeca26f77e1e8a49
邢阳 <xingyang@xingyangdeMacBook-Pro.local>
2023-01-18 15:49:41 -0800

    graphql

--- src/App.js
+++ src/App.js
@@ -28,16 +28,14 @@ const App = ({ signOut }) => {
   async function fetchNotes() {
     const apiData = await API.graphql({ query: listNotes });
     const notesFromAPI = apiData.data.listNotes.items;
-    await Promise.all(
-      notesFromAPI.map(async (note) => {
-        if (note.image) {
-          const url = await Storage.get(note.name);
-          note.image = url;
-        }
-        return note;
-      })
-    );
-    setNotes(notesFromAPI);
+    await Promise.all(notesFromAPI.map(async note => {
+      if (note.image) {
+        const image = await Storage.get(note.image);
+        note.image = image;
+      }
+      return note;
+    }))
+    setNotes(apiData.data.listNotes.items);
   }
 
   async function createNote(event) {
@@ -70,13 +68,13 @@ const App = ({ signOut }) => {
 
   return (
     <View className="App">
-      <Heading level={1}>My Notes App</Heading>
       <View
         name = "image"
         as = "input"
         type = "file"
         style = {{ alignSelf: "end" }}
-      />      
+      />
+      <Heading level={1}>My Notes App</Heading>      
       <View as="form" margin="3rem 0" onSubmit={createNote}>
         <Flex direction="row" justifyContent="center">
           <TextField
@@ -103,23 +101,23 @@ const App = ({ signOut }) => {
       <Heading level={2}>Current Notes</Heading>
       <View margin="3rem 0">
       {notes.map((note) => (
-        <Flex
-          key={note.id || note.name}
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Text as="strong" fontWeight={700}>
-            {note.name}
-          </Text>
-          <Text as="span">{note.description}</Text>
-            {note.image && (
-          <Image
-            src={note.image}
-            alt={`visual aid for ${notes.name}`}
-            style={{ width: 400 }}
-          />
-   )}
+  <Flex
+    key={note.id || note.name}
+    direction="row"
+    justifyContent="center"
+    alignItems="center"
+  >
+    <Text as="strong" fontWeight={700}>
+      {note.name}
+    </Text>
+    <Text as="span">{note.description}</Text>
+    {note.image && (
+      <Image
+        src={note.image}
+        alt={`visual aid for ${notes.name}`}
+        style={{ width: 400 }}
+      />
+    )}
     <Button variation="link" onClick={() => deleteNote(note)}>
       Delete note
     </Button>
